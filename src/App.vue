3f8e78981338e6762b57961784dc7cc6aad856d5
VictorSedlak1 <victorsedlak04@gmail.com>
2023-06-05 11:51:18 -0300

    Pub_03

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,20 @@
 <script setup>
-import { livros } from '@/_data/livros.js'
 import MeuCarrinho from './components/MeuCarrinho.vue';
-import CardLivro from './components/CardLivro.vue';
+import ListagemLivro from './components/ListagemLivro.vue';
 
-function formatarPreco(preco) {
-  return 'R$ ' + preco.toFixed(2).replace('.', ',')
-}
+
 </script>
 
 <template>
   <h1>Minha livraria</h1>
   <div class="container-geral">
-    <div class="listagem-livros">
-      <card-livro v-for="livro in livros" :key="livro.id" :livro="livro"/>
-    </div>
     <meu-carrinho />
+    <listagem-livros />
   </div>
 </template>
 
 <style scoped>
-.wrap-carrinho .carrinho-total {
-  position: fixed;
-  bottom: 3%;
-  font-size: 1.5rem;
-  font-weight: bold;
-}
 
-.item-carrinho .info-livro {
-  display: flex;
-  margin-bottom: 10px;
-}
-.detalhes-livro {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-}
-.detalhes-livro p {
-  margin: 0;
-}
-.detalhes-livro div {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-}
-
-.detalhes-livro input[type='number'] {
-  width: 50px;
-  text-align: center;
-  border: none;
-  border-bottom: 1px solid black;
-  background-color: transparent;
-  margin-left: 10px;
-}
-
-.detalhes-livro button {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  font-size: 1.5rem;
-  color: black;
-  padding: 0;
-  margin: 0;
-}
-
-.info-livro-preco {
-  margin-left: auto;
-}
-.icon-capa-livro {
-  width: 30px;
-  margin-right: 10px;
-}
 .container-geral {
   /* display: flex;
   justify-content: space-between; */
@@ -77,12 +22,6 @@ function formatarPreco(preco) {
   grid-template-columns: 3fr 1fr;
 }
 
-.carrinho {
-  /* min-width: 20%; */
-}
-.listagem-livros {
-  display: flex;
-  flex-wrap: wrap;
-}
+
 
 </style>
--- src/components/CardLivro.vue
+++ src/components/CardLivro.vue
@@ -58,4 +58,57 @@ function formatarPreco(preco) {
   font-weight: bold;
   margin-bottom: 5px;
 }
+
+
+.wrap-carrinho .carrinho-total {
+  position: fixed;
+  bottom: 3%;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.item-carrinho .info-livro {
+  display: flex;
+  margin-bottom: 10px;
+}
+.detalhes-livro {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.detalhes-livro p {
+  margin: 0;
+}
+.detalhes-livro div {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.detalhes-livro input[type='number'] {
+  width: 50px;
+  text-align: center;
+  border: none;
+  border-bottom: 1px solid black;
+  background-color: transparent;
+  margin-left: 10px;
+}
+
+.detalhes-livro button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: black;
+  padding: 0;
+  margin: 0;
+}
+
+.info-livro-preco {
+  margin-left: auto;
+}
+.icon-capa-livro {
+  width: 30px;
+  margin-right: 10px;
+}
 </style>
